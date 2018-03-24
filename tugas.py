decab27fb7438c4849bee319de585381e39fcf9f
fona123 <37728894+fona123@users.noreply.github.com>
2018-03-24 14:51:13 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,26 @@
+from texttable import Texttable
+table = Texttable ()
+jawab = "y"
+no = 0
+nama = []
+nim = []
+nilai_tugas = []
+nilai_uts = []
+nilai_uas = []
+while( jawab == "y"):
+    nama.append(input("Masukan Nama  :"))
+    nim.append(input("Masukan Nim   :"))
+    nilai_tugas.append(input("Nilai Tugas   :"))
+    nilai_uts.append(input("Nilai UTS     :"))
+    nilai_uas.append(input("Nilai UAS     :"))
+    jawab = input("Tambah data (y/t)?")
+    no += 1
+for i in range(no):
+    tugas = int(nilai_tugas[i])
+    uts = int(nilai_uts[i])
+    uas = int(nilai_uas[i])
+    akhir = (tugas*30/100) + (uts*35/100) + (uas*35/100)
+    table.add_rows([['No','Nama','Nim','Tugas','UTS','UAS','AKHIR'],
+                    [i+1, nama[i],nim[i],nilai_tugas[i],nilai_uts[i],nilai_uas[i],akhir]])
+print (table.draw())
+                    
